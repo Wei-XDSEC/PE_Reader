b3214f32177b6e4d1aff77044427e1b9907bdd8c
Wei-XDSEC <38145852+Wei-XDSEC@users.noreply.github.com>
2018-05-10 19:40:43 +0800

    Update NT_HEADERS.cpp

--- NT_HEADERS.cpp
+++ NT_HEADERS.cpp
@@ -12,18 +12,28 @@ int MY_NT_HEADERS::Read(FILE *fp)
 int MY_NT_HEADERS::Paint()
 {
     using namespace std;
-    cout << "\n__________________________________NT Headers:\n";
-    cout << "_________________Signature:\n";
-    cout << setw(11) << "Offset";
-    cout << setw(3) << "Content\n";
-    cout << "------------------" << endl;
-    int i, Size = 4;
+    int i;
     char *content = this->m_pcontent;
+
+    cout << "\nNT Headers: Signature:\n";
+    cout << "_______________________________________________________________" << endl;
+    cout << setw(11) << "Offset";
+    cout << setw(33) << "Name of member";
+    cout << setw(14) << "Type";
+    cout << setw(3) << "Value\n";
+    cout << "---------------------------------------------------------------" << endl;
     cout << setfill('0') << setiosflags(ios::right);
     cout << "0x" << setw(8) << hex << (LONG)(MY_DOS_STUB::m_lpOffset_Of_NT_header) << ' ';
-    for(i = 0; i < Size; i++)
-        cout << setw(2) << hex << (unsigned int)(unsigned char)*content++ << ' ';
-    cout << "#Signature:\"PE\"\n";
+    cout << setfill(' ') <<resetiosflags(ios::right);
+    cout << setw(33) << "Signature";
+    cout << setw(14) << "DWORD";
+    cout << setfill('0') << setiosflags(ios::right);
+    content += sizeof(DWORD)-1;
+    for(i = 0; i < sizeof(DWORD); i++)
+        cout << setw(2) << hex << (unsigned int)(unsigned char)*(content-i);
+    content += 1;
+    if(*(reinterpret_cast<DWORD*>(this->m_pcontent)) == 0x4550)
+        cout << " #Signature:\"PE\"\n";
     cout << setfill(' ') << resetiosflags(ios::right) <<  endl;
     return 1;
 }
@@ -60,24 +70,27 @@ int MY_FILE_HEADER::Paint()
     unsigned int i, j;
     LONG offset = MY_DOS_STUB::m_lpOffset_Of_NT_header+SIZE_OF_SIGNATURE;
     char *content = reinterpret_cast<char *>(this->m_pFile_header);
-    cout << "_________________File header:\n";
+    cout << "\nNT header: File header:\n";
     cout << setiosflags(ios::left);
+    cout << "_______________________________________________________________" << endl;
     cout << setw(11) << "Offset";
-    cout << setw(22) << "Name of member";
-    cout << setw(10) << "Type";
-    cout << setw(3) << "Content\n";
-    cout << "--------------------------------------------------" << endl;
+    cout << setw(33) << "Name of member";
+    cout << setw(14) << "Type";
+    cout << setw(3) << "Value\n";
+    cout << "---------------------------------------------------------------" << endl;
     for(i = 0; i < NUMBER_OF_FILE_HEADER; i++)
     {
         cout << setfill('0') << setiosflags(ios::right);
         cout << "0x" << setw(8) << hex << (LONG)offset << ' ';
         cout<<resetiosflags(ios::right);
         cout << setfill(' ');
-        cout << setw(22) << MY_FILE_HEADER::m_pszNameTable[i];
-        cout << setw(10) << MY_FILE_HEADER::m_pszTypeTable[i];
+        cout << setw(33) << MY_FILE_HEADER::m_pszNameTable[i];
+        cout << setw(14) << MY_FILE_HEADER::m_pszTypeTable[i];
         cout << setfill('0') << setiosflags(ios::right);
+        content += MY_FILE_HEADER::m_pnSizeTable[i] - 1;
         for(j = 0; j < MY_FILE_HEADER::m_pnSizeTable[i]; j++)
-            cout << setw(2) << hex << (unsigned int)(unsigned char)*content++ << ' ';
+            cout << setw(2) << hex << (unsigned int)(unsigned char)*(content-j);
+        content += 1;
         cout<<resetiosflags(ios::right);
        // Comment(i);
         cout << endl;
@@ -90,18 +103,38 @@ int MY_FILE_HEADER::Paint()
 
 const char *MY_OPTIONAL_HEADER::m_pszTypeTable32[]=
 {
+    "WORD", "BYTE", "BYTE", "DWORD",
+    "DWORD", "DWORD", "DWORD", "DWORD",
+    "DWORD", "DWORD", "DWORD", "DWORD",
+    "WORD", "WORD", "WORD", "WORD",
     "WORD", "WORD", "DWORD", "DWORD",
-    "DWORD", "WORD", "WORD"
+    "DWORD", "DWORD", "WORD", "WORD",
+    "DWORD", "DWORD", "DWORD", "DWORD",
+    "DWORD", "DWORD"
 };
+
 const char *MY_OPTIONAL_HEADER::m_pszNameTable32[]=
 {
-    "Machine", "NumberOfSections", "TimeDataStamp", "PointerToSymbolTable",
-    "NumberOfSymbols", "SizeOfOptionalHeader", "Characteristics"
+    "Magic", "MajorLinkerVersion", "MinorLinkerVersion", "SizeOfCode",
+    "SizeOfInitializedData", "SizeOfUninitializedData", "AddressOfEntryPoint", "BaseOfCode",
+    "BaseOfData", "ImageBase", "SectionAlignment", "FileAlignment",
+    "MajorOperatingSystemVersion", "MinorOperatingSystemVersion", "MajorImageVersion", "MinorImageVersion",
+    "MajorSubsystemVersion", "MinorSubsystemVersion", "Win32VersionValue", "SizeOfImage",
+    "SizeOfHeaders", "CheckSum", "Subsystem", "DllCharacteristics",
+    "SizeOfStackReserve", "SizeOfStackCommit", "SizeOfHeapReserve", "SizeOfHeapCommit",
+    "LoaderFlags", "NumberOfRvaAndSizes"
 };
+
 const size_t MY_OPTIONAL_HEADER::m_pnSizeTable32[]=
 {
-    sizeof(WORD), sizeof(WORD), sizeof(DWORD), sizeof(DWORD),
-    sizeof(DWORD), sizeof(WORD), sizeof(WORD)
+    sizeof(WORD), sizeof(BYTE), sizeof(BYTE), sizeof(DWORD),
+    sizeof(DWORD), sizeof(DWORD), sizeof(DWORD), sizeof(DWORD),
+    sizeof(DWORD), sizeof(DWORD), sizeof(DWORD), sizeof(DWORD),
+    sizeof(DWORD), sizeof(WORD), sizeof(WORD), sizeof(WORD),
+    sizeof(WORD), sizeof(WORD), sizeof(WORD), sizeof(DWORD),
+    sizeof(DWORD), sizeof(DWORD), sizeof(WORD), sizeof(WORD),
+    sizeof(DWORD), sizeof(DWORD), sizeof(DWORD), sizeof(DWORD),
+    sizeof(DWORD), sizeof(DWORD)
 };
 
 const char *MY_OPTIONAL_HEADER::m_pszTypeTable64[]=
@@ -115,6 +148,7 @@ const char *MY_OPTIONAL_HEADER::m_pszTypeTable64[]=
     "ULONGLONG", "ULONGLONG", "ULONGLONG", "DWORD",
     "DWORD"
 };
+
 const char *MY_OPTIONAL_HEADER::m_pszNameTable64[]=
 {
     "Magic", "MajorLinkerVersion", "MinorLinkerVersion", "SizeOfCode",
@@ -127,6 +161,7 @@ const char *MY_OPTIONAL_HEADER::m_pszNameTable64[]=
     "NumberOfRvaAndSizes"
 
 };
+
 const size_t MY_OPTIONAL_HEADER::m_pnSizeTable64[]=
 {
     sizeof(WORD), sizeof(BYTE), sizeof(BYTE), sizeof(DWORD),
@@ -163,32 +198,71 @@ int MY_OPTIONAL_HEADER::Read(FILE *fp)
 int MY_OPTIONAL_HEADER::Paint()
 {
     using namespace std;
-    char *content = reinterpret_cast<char *>(this->m_pOptional_header64);
-    unsigned int i, j, NumberOfRvaAndSizes = this->m_pOptional_header64->NumberOfRvaAndSizes;
+    char *content;
+    unsigned int i, j, NumberOfRvaAndSizes;
+    if(this->Is64)
+    {
+        content = reinterpret_cast<char *>(this->m_pOptional_header64);
+        NumberOfRvaAndSizes = this->m_pOptional_header64->NumberOfRvaAndSizes;
+    }
+    else
+    {
+        content = reinterpret_cast<char *>(this->m_pOptional_header32);
+        NumberOfRvaAndSizes = this->m_pOptional_header32->NumberOfRvaAndSizes;
+    }
+
     LONG offset = MY_DOS_STUB::m_lpOffset_Of_NT_header+SIZE_OF_SIGNATURE+SIZE_OF_FILE_HEADER;
-    cout << "_________________Optional header:\n";
+    cout << "NT headers: Optional header:\n";
     cout << setiosflags(ios::left);
+    cout << "_______________________________________________________________" << endl;
     cout << setw(11) << "Offset";
     cout << setw(33) << "Name of member";
-    cout << setw(10) << "Type";
-    cout << setw(3) << "Content\n";
-    cout << "--------------------------------------------------" << endl;
-    for(i = 0; i < NUMBER_OF_OPTIONAL_HEADER64-1; i++)
+    cout << setw(14) << "Type";
+    cout << setw(3) << "Value\n";
+    cout << "---------------------------------------------------------------" << endl;
+    if(this->Is64)
     {
-        cout << setfill('0') << setiosflags(ios::right);
-        cout << "0x" << setw(8) << hex << (LONG)offset << ' ';
-        cout<<resetiosflags(ios::right);
-        cout << setfill(' ');
-        cout << setw(33) << MY_OPTIONAL_HEADER::m_pszNameTable64[i];
-        cout << setw(10) << MY_OPTIONAL_HEADER::m_pszTypeTable64[i];
-        cout << setfill('0') << setiosflags(ios::right);
-        for(j = 0; j < MY_OPTIONAL_HEADER::m_pnSizeTable64[i]; j++)
-            cout << setw(2) << hex << (unsigned int)(unsigned char)*content++ << ' ';
-        cout<<resetiosflags(ios::right);
-       // Comment(i);
-        cout << endl;
-        offset += MY_OPTIONAL_HEADER::m_pnSizeTable64[i];
+        for(i = 0; i < NUMBER_OF_OPTIONAL_HEADER64-1; i++)
+        {
+            cout << setfill('0') << setiosflags(ios::right);
+            cout << "0x" << setw(8) << hex << (LONG)offset << ' ';
+            cout<<resetiosflags(ios::right);
+            cout << setfill(' ');
+            cout << setw(33) << MY_OPTIONAL_HEADER::m_pszNameTable64[i];
+            cout << setw(14) << MY_OPTIONAL_HEADER::m_pszTypeTable64[i];
+            cout << setfill('0') << setiosflags(ios::right);
+            content += MY_OPTIONAL_HEADER::m_pnSizeTable64[i] - 1;
+            for(j = 0; j < MY_OPTIONAL_HEADER::m_pnSizeTable64[i]; j++)
+                cout << setw(2) << hex << (unsigned int)(unsigned char)*(content-j);
+            content += 1;
+            cout<<resetiosflags(ios::right);
+           // Comment(i);
+            cout << endl;
+            offset += MY_OPTIONAL_HEADER::m_pnSizeTable64[i];
+        }
     }
+    else
+    {
+        for(i = 0; i < NUMBER_OF_OPTIONAL_HEADER32-1; i++)
+        {
+            cout << setfill('0') << setiosflags(ios::right);
+            cout << "0x" << setw(8) << hex << (LONG)offset << ' ';
+            cout<<resetiosflags(ios::right);
+            cout << setfill(' ');
+            cout << setw(33) << MY_OPTIONAL_HEADER::m_pszNameTable32[i];
+            cout << setw(14) << MY_OPTIONAL_HEADER::m_pszTypeTable32[i];
+            cout << setfill('0') << setiosflags(ios::right);
+            content += MY_OPTIONAL_HEADER::m_pnSizeTable32[i] - 1;
+            for(j = 0; j < MY_OPTIONAL_HEADER::m_pnSizeTable32[i]; j++)
+                cout << setw(2) << hex << (unsigned int)(unsigned char)*(content-j);
+            content += 1;
+            cout<<resetiosflags(ios::right);
+           // Comment(i);
+            cout << endl;
+            offset += MY_OPTIONAL_HEADER::m_pnSizeTable32[i];
+        }
+    }
+
     cout << setfill('0') << setiosflags(ios::right);
     cout << "0x" << setw(8) << hex << (LONG)offset << ' ';
     cout << setfill(' ') << resetiosflags(ios::right);
@@ -202,10 +276,12 @@ int MY_OPTIONAL_HEADER::Paint()
         cout << setfill(' ');
         cout << setw(8) << "RVA of ";
         cout << setw(25) << MY_OPTIONAL_HEADER::m_pszDataDirectoryTable[i];
-        cout << setw(10) << "DWORD";
+        cout << setw(14) << "DWORD";
         cout << setfill('0') << setiosflags(ios::right);
+        content += sizeof(DWORD) - 1;
         for(j = 0; j < sizeof(DWORD); j++)
-            cout << setw(2) << hex << (unsigned int)(unsigned char)*content++ << ' ';
+            cout << setw(2) << hex << (unsigned int)(unsigned char)*(content-j);
+        content += 1;
         cout<<resetiosflags(ios::right) << endl;
         offset += sizeof(DWORD);
         cout << setfill('0') << setiosflags(ios::right);
@@ -214,10 +290,12 @@ int MY_OPTIONAL_HEADER::Paint()
         cout << setfill(' ');
         cout << setw(8) << "size of ";
         cout << setw(25) << MY_OPTIONAL_HEADER::m_pszDataDirectoryTable[i];
-        cout << setw(10) << "DWORD";
+        cout << setw(14) << "DWORD";
         cout << setfill('0') << setiosflags(ios::right);
+        content += sizeof(DWORD) - 1;
         for(j = 0; j < sizeof(DWORD); j++)
-            cout << setw(2) << hex << (unsigned int)(unsigned char)*content++ << ' ';
+            cout << setw(2) << hex << (unsigned int)(unsigned char)*(content-j);
+        content += 1;
         cout<<resetiosflags(ios::right) << endl;
         offset += sizeof(DWORD);
     }
